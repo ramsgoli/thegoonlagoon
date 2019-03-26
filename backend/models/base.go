a9857cab09171746e82356bf44b974f14096873f
Ram Goli <ramsgoli@gmail.com>
2019-03-25 22:55:01 -0700

    remove dependency on env files

--- backend/models/base.go
+++ backend/models/base.go
@@ -4,19 +4,12 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/joho/godotenv"
 	"os"
 )
 
 var db *gorm.DB
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
 	host := os.Getenv("DB_HOST")
 	name := os.Getenv("DB_NAME")
 	user := os.Getenv("DB_USER")
